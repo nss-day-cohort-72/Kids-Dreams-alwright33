f31a05624253570d024cb5725dd004d24414aa24
alwright33 <austinlucaswright@gmail.com>
2024-08-14 16:02:04 -0500

    added functioning event listeners to kids and celebrityList

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -2,6 +2,22 @@ import { getCelebrities } from "./database.js"
 
 const celebrities = getCelebrities()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        const celebrityId = itemClicked.dataset.id
+
+        if (itemClicked.dataset.type === "celebrity") {
+            for (const celebrity of celebrities) {
+                if (celebrity.id === parseInt(celebrityId)) {
+                    window.alert(`${itemClicked.textContent} is a ${celebrity.sport} star.`)
+                } 
+            }
+        }
+    }
+)
+
 export const Celebrities = () => {
     let html = "<ol>"
 
--- scripts/Kids.js
+++ scripts/Kids.js
@@ -2,6 +2,22 @@ import { getChildren } from "./database.js"
 
 const children = getChildren()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        const kidId = itemClicked.dataset.id
+
+        if (itemClicked.dataset.type === 'child') {
+            for (const child of children) {
+                if (child.id === parseInt(kidId)) {
+                    window.alert(`${itemClicked.textContent} wish is ${child.wish}.`)
+                }
+            }
+        }
+    }
+)
+
 export const Kids = () => {
     let html = "<ol>"
 
